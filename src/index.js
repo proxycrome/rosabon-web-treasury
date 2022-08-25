1227ec90fa54d54792242373736db5a947967492
victoreze22 <victor.ezeonwuka@optisoft.ng>
2022-08-25 12:59:44 +0100

    styling fixed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './asset/scss/custom.scss'
 import '@fontsource/montserrat'
 import './index.css'
-import './asset/scss/custom.scss'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
@@ -10,7 +10,7 @@ import { Provider } from 'react-redux'
 import configureStore from './redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
-import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/css/bootstrap.css'
 // import 'mdbreact/dist/css/mdb.css';
 import 'react-toastify/dist/ReactToastify.css'
 
